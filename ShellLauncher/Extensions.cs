b953f28c37b5864d9d46f48d68316e66db163df9
GregAskew <gaskew7@gmail.com>
2019-03-03 15:29:27 -0500

    Initial commit

---
+++ ShellLauncher/Extensions.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+namespace ShellLauncher {
+
+    #region Usings
+    using System.Diagnostics;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks; 
+    #endregion
+
+    internal static class Extensions {
+        /// <summary>
+        /// Gets the friendly formatted text value of the SqlException
+        /// </summary>
+        /// <param name="e">The Exception</param>
+        /// <returns>The exception details</returns>
+        [DebuggerStepThroughAttribute]
+        private static string LogSqlErrors(SqlException e) {
+            var error = new StringBuilder();
+            for (int index = 0; index < e.Errors.Count; index++) {
+                error.AppendLine();
+                error.AppendLine("SqlException:");
+                error.AppendLine($"Line Number: {e.Errors[index].LineNumber}");
+                error.AppendLine($"Message: {e.Errors[index].Message}");
+                error.AppendLine($"Error Number: {e.Errors[index].Number}");
+                error.AppendLine($"Procedure: {e.Errors[index].Procedure}");
+                error.AppendLine($"Server: {e.Errors[index].Server}");
+                error.AppendLine($"Source: {e.Errors[index].Source}");
+            }
+
+            return error.ToString();
+        }
+
+        /// <summary>
+        /// Stack trace, target site, and error message of outer and inner exception, formatted with newlines
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="exception"></param>
+        /// <returns></returns>
+        [DebuggerStepThroughAttribute]
+        internal static string VerboseExceptionString<T>(this T exception) where T : Exception {
+            var exceptionString = new StringBuilder();
+
+            exceptionString.AppendLine($" Exception: {exception.GetType().Name} Message: {exception.Message ?? "NULL"}");
+            exceptionString.AppendLine($" StackTrace: {exception.StackTrace ?? "NULL"}");
+            exceptionString.AppendLine($" TargetSite: {(exception.TargetSite != null ? exception.TargetSite.ToString() : "NULL")}");
+
+            if (exception as SqlException != null) {
+                exceptionString.AppendLine(LogSqlErrors(exception as SqlException));
+            }
+
+            if (exception.InnerException != null) {
+                exceptionString.AppendLine();
+                exceptionString.AppendLine("Inner Exception:");
+                exceptionString.AppendLine(exception.InnerException.VerboseExceptionString());
+            }
+
+            return exceptionString.ToString();
+        }
+    }
+}
---
+++ ShellLauncher/Program.cs
@@ -0,0 +1,143 @@
+﻿namespace ShellLauncher {
+
+    #region Usings
+    using System;
+    using System.Collections.Generic;
+    using System.Configuration;
+    using System.Diagnostics;
+    using System.IO;
+    using System.Linq;
+    using System.Security.Principal;
+    using System.Text;
+    using System.Threading.Tasks;
+    #endregion
+
+    /// <summary>
+    /// Launches an alternate shell application.
+    /// Users with the alternate shell are logged off when the application is closed.
+    /// Builtin Administrator account and Domain Admins are excluded from alternate shell and load Explorer.exe.
+    ///
+    /// To use, copy ShellLauncher.exe and ShellLauncher.exe.config to %systemroot%, and set the following registry value:
+    /// Key: HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon
+    /// Value: Shell
+    /// Value Type: REG_SZ
+    /// Value Data: %systemroot%\ShellLauncher.exe
+    /// </summary>
+    internal class Program {
+
+        #region Members
+
+        private static IReadOnlyList<string> ExclusionGroups { get; set; }
+
+        private static bool LogGroupMembershipsToEventLog { get; set; }
+
+        private static string ShellApplication { get; set; }
+
+        private static IReadOnlyList<NTAccount> WindowsGroups { get; set; }
+
+        #endregion
+
+        private static void Initialize(string[] args) {
+
+            Console.CancelKeyPress += (object sender, ConsoleCancelEventArgs e) => {
+                e.Cancel = true;
+            };
+
+            ExclusionGroups = new List<string>();
+            WindowsGroups = new List<NTAccount>();
+
+            var windowsGroups = new List<NTAccount>();
+            foreach (var identityReference in WindowsIdentity.GetCurrent().Groups) {
+                NTAccount ntAccount = null;
+                try {
+                    ntAccount = identityReference.Translate(typeof(NTAccount)) as NTAccount;
+                    if (ntAccount != null) {
+                        windowsGroups.Add(ntAccount);
+                    }
+                }
+                catch { }
+            }
+            WindowsGroups = windowsGroups;
+
+            if (ConfigurationManager.AppSettings["ExclusionGroups"] != null) {
+                var exclusionGroups = ConfigurationManager.AppSettings["ExclusionGroups"].Split(new char[] { '|' });
+                ExclusionGroups = exclusionGroups.Select(x => x.Trim()).Distinct().OrderBy(x => x).ToList();
+            }
+
+            if (!string.IsNullOrWhiteSpace(ConfigurationManager.AppSettings["LogGroupMembershipsToEventLog"])) {
+                LogGroupMembershipsToEventLog = Convert.ToBoolean(ConfigurationManager.AppSettings["LogGroupMembershipsToEventLog"]);
+            }
+
+            if (LogGroupMembershipsToEventLog) {
+                EventLog.WriteEntry("Application", $"ShellLauncher: Identity: {WindowsIdentity.GetCurrent().Name} Groups: {Environment.NewLine}{string.Join(Environment.NewLine, windowsGroups.Select(x => x.Value))}", EventLogEntryType.Information);
+            }
+
+            if (WindowsGroups.Any(x => ExclusionGroups.Any(y => string.Equals(x.Value, y, StringComparison.OrdinalIgnoreCase)))
+                || WindowsIdentity.GetCurrent().User.IsWellKnown(WellKnownSidType.BuiltinAdministratorsSid)
+                || WindowsIdentity.GetCurrent().User.IsWellKnown(WellKnownSidType.AccountDomainAdminsSid)) {
+                ShellApplication = Path.Combine(Environment.ExpandEnvironmentVariables("%systemroot%"), "explorer.exe");
+            }
+            else {
+                if (ConfigurationManager.AppSettings["ShellApplication"] != null) {
+                    ShellApplication = ConfigurationManager.AppSettings["ShellApplication"].Trim();
+                }
+                var shellApplicationParameter = args
+                    .Where(x => x.StartsWith("ShellApplication:"))
+                    .FirstOrDefault();
+                if (!string.IsNullOrWhiteSpace(shellApplicationParameter)) {
+                    ShellApplication = shellApplicationParameter.Substring("ShellApplication:".Length).Trim();
+                }
+
+                if (string.IsNullOrWhiteSpace(ShellApplication)) {
+                    throw new ApplicationException("ShellApplication not specified");
+                }
+                if (!File.Exists(ShellApplication)) {
+                    throw new ApplicationException($"ShellApplication does not exist: {ShellApplication}");
+                }
+            }
+        }
+
+        static void Main(string[] args) {
+
+            AppDomain.CurrentDomain.UnhandledException += new UnhandledExceptionEventHandler(CurrentDomain_UnhandledException);
+
+            Initialize(args);
+
+            var processStartInfo = new ProcessStartInfo();
+            processStartInfo.UseShellExecute = false;
+            processStartInfo.WindowStyle = ProcessWindowStyle.Maximized;
+            processStartInfo.CreateNoWindow = false;
+            processStartInfo.FileName = ShellApplication;
+
+            var process = new Process();
+            process.StartInfo = processStartInfo;
+            process.Start();
+            process.WaitForExit();
+
+            if (!WindowsGroups.Any(x => ExclusionGroups.Any(y => string.Equals(x.Value, y, StringComparison.OrdinalIgnoreCase)))
+                && !WindowsIdentity.GetCurrent().User.IsWellKnown(WellKnownSidType.BuiltinAdministratorsSid)
+                && !WindowsIdentity.GetCurrent().User.IsWellKnown(WellKnownSidType.AccountDomainAdminsSid)) {
+                processStartInfo = new ProcessStartInfo();
+                processStartInfo.UseShellExecute = false;
+                processStartInfo.WindowStyle = ProcessWindowStyle.Minimized;
+                processStartInfo.CreateNoWindow = false;
+                var fileName = Path.Combine(Environment.ExpandEnvironmentVariables("%systemroot%"), "system32", "logoff.exe");
+                processStartInfo.FileName = fileName;
+
+                process = new Process();
+                process.StartInfo = processStartInfo;
+                process.Start();
+            }
+        }
+
+        /// <summary>
+        /// Unhandled Exception Handler
+        /// </summary>
+        /// <param name="sender">Sender</param>
+        /// <param name="e">Event Args</param>
+        private static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e) {
+            var exception = e.ExceptionObject as Exception;
+            EventLog.WriteEntry("Application", exception.VerboseExceptionString(), EventLogEntryType.Error);
+        }
+    }
+}
